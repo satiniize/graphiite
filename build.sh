77b78df6323af40a8badea67294a4b5cb49e789b
Rayhan Nurdjaman <rayhansat1210@gmail.com>
2026-04-12 23:39:45 +0800

    More code cleanup

--- build.sh
+++ build.sh
@@ -1,29 +1,11 @@
 #!/bin/bash
 set -e
 
-# Default build type
-BUILD_TYPE="Release" # <-- Set default to Release
+BUILD_TYPE="${1:-Release}"
 
-# Check for an argument to override the build type
-if [ -n "$1" ]; then # Checks if the first argument ($1) is non-empty
-    BUILD_TYPE="$1"
-fi
+echo "Building: $BUILD_TYPE"
 
-# Convert to title case for CMake compatibility (e.g., "debug" -> "Debug")
-BUILD_TYPE=$(echo "$BUILD_TYPE" | awk '{print toupper(substr($0,1,1))tolower(substr($0,2))}')
-
-echo "Building with configuration: $BUILD_TYPE"
-
-# Run your shader script (if it's independent of build type)
 ./compile_spv.sh
 
-# Configure the project with CMake
-# The -D flag sets a CMake variable, overriding the default
 cmake -S . -B build -G Ninja -DCMAKE_BUILD_TYPE="$BUILD_TYPE"
-
-# Build the project
-# --config flag ensures we build the specific configuration
 cmake --build build --config "$BUILD_TYPE"
-
-# # Run the binary (commented out as Zed will handle this)
-# ./build/"$BUILD_TYPE"/software-renderer
