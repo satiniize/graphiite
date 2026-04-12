566dd4d49969ed49ebc16e33d208d0eef562dd15
Rayhan Nurdjaman <rayhansat1210@gmail.com>
2026-04-12 11:58:20 +0800

    Increased scroll speed and reduced offset range

--- include/input_manager.hpp
+++ include/input_manager.hpp
@@ -9,7 +9,7 @@ public:
   Clay_Vector2 mouse_position = {0.0f, 0.0f};
   Clay_Vector2 mouse_scroll = {0.0f, 0.0f};
   bool is_mouse_down = false;
-  float scroll_speed = 1.0f;
+  float scroll_speed = 2.0f;
 
   InputManager() {}
   void poll_events(bool *running);
